16a4263a8e3e0976c70fc8febed42a550f35845e
GenjutsYou <125905469+GenjutsYou@users.noreply.github.com>
2023-08-06 05:33:23 -0700

    set up models

---
+++ models/Thought.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new mongoose.Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => new Date(timestamp).toISOString(),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+module.exports = Thought;
